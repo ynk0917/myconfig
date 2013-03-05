7b992e6fea200482f6d299d9024b8acd9aa5107c
NienKun Yeh <akun@akun-desktop.(none)>
2013-03-05 13:57:35 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 current_directory=`pwd`
 if [ -f ~/.bashrc ];then
+    rm ~/.bashrc.bak
     mv ~/.bashrc ~/.bashrc.bak
-    rm ~/.bashrc
 fi
 ln -s $current_directory/bashrc ~/.bashrc
 if [ -f ~/.gitconfig ];then
+    rm ~/.gitconfig.bak
     mv ~/.gitconfig ~/.gitconfig.bak
-    rm ~/.gitconfig
 fi
 ln -s $current_directory/gitconfig ~/.gitconfig
 if [ -d ~/.config/awesome ];then
+    rm -rf ~/.config/awesome_bak
     mv ~/.config/awesome ~/.config/awesome_bak
 fi
 ln -s $current_directory/awesome ~/.config/awesome
