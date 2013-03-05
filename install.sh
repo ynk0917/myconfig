2e321149b46f0f6250eaca48cabf4d5f515227a2
NienKun Yeh <ynk0917@gmail.com>
2013-03-05 11:53:29 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,18 @@
 current_directory=`pwd`
 if [ -f ~/.bashrc ];then
     mv ~/.bashrc ~/.bashrc.bak
+    rm ~/.bashrc
 fi
 ln -s $current_directory/bashrc ~/.bashrc
 if [ -f ~/.gitconfig ];then
     mv ~/.gitconfig ~/.gitconfig.bak
+    rm ~/.gitconfig
 fi
 ln -s $current_directory/gitconfig ~/.gitconfig
-cd awesome
-./install.sh
-cd ..
 if [ -d ~/.config/awesome ];then
     mv ~/.config/awesome ~/.config/awesome_bak
 fi
+ln -s $current_directory/awesome ~/.config/awesome
 git submodule init
 git submodule update
 git submodule foreach git submodule update
