99bccb3f2d83c0d3a990d4a17595828e113c29af
NienKun Yeh <ynk0917@gmail.com>
2013-03-05 10:32:03 +0800

    add more config.
    add bashrc
    add gitconfig

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-awesome/install.sh
+if [ -f ~/.bashrc ];then
+    mv ~/.bashrc ~/.bashrc.bak
+fi
+ln -s bashrc ~/.bashrc
+if [ -f ~/.gitconfig ];then
+    mv ~/.gitconfig ~/.gitconfig.bak
+fi
+ln -s gitconfig ~/.gitconfig
+cd awesome
+./install.sh
+cd ..
+if [ -d ~/.config/awesome ];then
+    mv ~/.config/awesome ~/.config/awesome_bak
+fi
+git submodule foreach git submodule update
 vim/install.sh
